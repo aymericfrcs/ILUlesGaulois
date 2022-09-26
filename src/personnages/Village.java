74aa6a69c12a7d33bcf0c20bd16fd6f54b148c3a
frances aymeric <frm4465a@ens.fsi>
2022-09-26 19:41:01 +0200

    TP1

---
+++ src/personnages/Village.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	private Chef chef;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}
