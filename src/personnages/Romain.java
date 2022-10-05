fb4cdd33abeb90547b3f1316cf7c7edc69e3b993
frances aymeric <frm4465a@ens.fsi>
2022-10-05 15:16:03 +0200

    TP2: fin page 6

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -6,6 +6,7 @@ public class Romain {
 	
 	public Romain(String nom, int force) {
 		super();
+		assert force > 0;
 		this.nom = nom;
 		this.force = force;
 	}
@@ -24,16 +25,19 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert force > 0; //la force d'un romain est positive
+		int forceAvant = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert forceAvant < force; //la force du romain a diminuée
 	}
 	
 	public static void main(String[] args) {
-		Romain minus = new Romain("Minus",8);
+		Romain minus = new Romain("Minus",6);
 		
 		minus.prendreParole();
 		minus.parler("Je parle !");
